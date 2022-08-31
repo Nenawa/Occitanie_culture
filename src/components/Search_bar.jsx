dfcf4e7af0de998a5dbf4f451dd1736a03747d91
Rémi derathe <remi.derathe@gmail.com>
2022-08-31 11:24:18 +0200

    Search bar v1 sans css

--- src/components/Search_bar.jsx
+++ src/components/Search_bar.jsx
@@ -1,6 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 function Search_bar() {
+    const searchRef = useRef(null);
+    const communeRef = useRef(null);
+    const typeRef = useRef(null);
+    const dateRef = useRef(null);
 
     //TODO: replace fetch by props
     const [data, setData] = useState(null);
@@ -14,18 +18,30 @@ function Search_bar() {
 
     const types = data?.facet_groups.filter(elm => elm.name === "type")[0].facets
     const communes = data?.facet_groups.filter(elm => elm.name === "commune")[0].facets
+
+    function handleChange() {
+        const link = `https://data.laregion.fr/api/records/1.0/search/?dataset=agendas-participatif-des-sorties-en-occitanie${searchRef.current.value ? `&q=${searchRef.current.value.replaceAll(" ", "%20")}` : ""}&lang=fr&facet=type&facet=thematique&facet=date_debut&facet=commune${communeRef.current.value ? `&refine.commune=${communeRef.current.value.replaceAll(" ", "%20")}` : ""}${typeRef.current.value ? `&refine.type=${typeRef.current.value.replaceAll(" ", "%20")}` : ""}${dateRef.current.value ? `&refine.start_date=${dateRef.current.value}` : ""}`
+
+        fetch(link)
+            .then((rep) => rep.json())
+            .then(rep => {
+                console.log(rep)
+            })
+
+    }
+
     return (
         <div>
-            <input type="text" placeholder="Rechercher ..."></input>
-            <select>
-                <option>Commune</option>
+            <input onChange={handleChange} ref={searchRef} type="text" placeholder="Rechercher ..."></input>
+            <select onChange={handleChange} ref={communeRef}>
+                <option value="">Commune</option>
                 {communes?.map((elm) => <option key={elm.name} title={elm.name}>{elm.name.length > 20 ? `${elm.name.slice(0, 20)}...` : elm.name}</option>)}
             </select>
-            <select>
-                <option>Type d'évènement</option>
-                {types?.map((elm) => <option key={elm.name} title={elm.name}>{elm.name.length > 20 ? `${elm.name.slice(0, 20)}...` : elm.name}</option>)}
+            <select onChange={handleChange} ref={typeRef}>
+                <option value="">Type d'évènement</option>
+                {types?.map((elm) => <option key={elm.name} title={elm.name} value={elm.name}>{elm.name.length > 20 ? `${elm.name.slice(0, 20)}...` : elm.name}</option>)}
             </select>
-            <input type="date"></input>
+            <input onChange={handleChange} ref={dateRef} type="date"></input>
         </div>
     )
 }
