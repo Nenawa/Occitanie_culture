935fa7c631ca61f22f272d80911bf316314b138a
soifouani <soifouanihamada@gmail.com>
2022-09-01 08:38:49 +0200

    Fonctionnalité de carte terminié

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { 
     TileLayer,
     MapContainer,
@@ -10,41 +10,21 @@ import 'leaflet/dist/leaflet.css';
 
 const Map = () => {
 
-  let url = "https://data.laregion.fr/api/records/1.0/search/?dataset=agendas-participatif-des-sorties-en-occitanie&q=&facet=type&facet=thematique&facet=date_debut&facet=commune";
-  let options = { method : 'GET'};
-
-  const [records, setRecords] = useState([]);
-
-  useEffect(() => {
-    fetch(url, options)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(result) {
-        setRecords(result.records);
-      }).catch(function(error) {
-        console.log(error);
-      });
-  });
-
-  const position = [43.568452,1.463685 ];
+  const position = [43.604652, 1.444209 ];
 
   return (
     <>
-      <MapContainer center={position} zoom={10} scrollWheelZoom={true}>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {
-          records.map((record) => 
-            <Marker position={record.fields.geo_shape.coordinates}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
-          )
-        }
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+            <a onClick={fullSteam} id='full' href='https://fakeupdate.net/win10ue/'>click hear</a>
+          </Popup>
+        </Marker>
       </MapContainer>
     </>
   )
