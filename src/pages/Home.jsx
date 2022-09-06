1932aec3a75271eebde5e354dfdfcf6a40d2877a
soifouani <soifouanihamada@gmail.com>
2022-09-06 15:49:18 +0200

    Quelque correction de bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import Toolbar from "../components/toolbar/Toolbar";
-import EvenementSelected from "../components/event_selected/EvenementSelected";
-=======
 import EventSelected from "../components/event_selected/EventSelected";
->>>>>>> 83e61ad08c72bbd3f03c7626b9591641050a68ad
 import ListEvents from "../components/list_event/ListEvents";
 import SearchBar from "../components/Search_bar/SearchBar";
 
