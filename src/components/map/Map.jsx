c6d7d58d113c1ca5dd4a3bcedb251430c90b26ac
remi.derathe <remi.derathe@gmail.com>
2022-09-22 12:17:17 +0200

    refresh position map

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
   TileLayer,
   MapContainer,
   Marker,
-  Popup
+  Popup,
+  useMap
 } from 'react-leaflet';
 
 import "leaflet/dist/leaflet.css";
 import "./map.css";
 
-const Map = () => {
+function ChangeView({ center, zoom }) {
+  const map = useMap();
+  map.setView(center, zoom);
+  return null;
+}
+
+const Map = (props) => {
 
   const URL = "https://fakeupdate.net/win10ue/";
 
@@ -17,17 +24,16 @@ const Map = () => {
     window.open(URL, "", "fullscreen=yes, scrollbars=auto");
   }
 
-  const position = [43.604652, 1.444209];
-
   return (
     <>
       <div className='map__container'>
-        <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+        <MapContainer center={[props.coordinates[1], props.coordinates[0]]} zoom={13} scrollWheelZoom={true}>
+          <ChangeView center={[props.coordinates[1], props.coordinates[0]]} zoom={13} /> 
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={position}>
+          <Marker position={[props.coordinates[1], props.coordinates[0]]}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
               <a onClick={fullScreen} href="#">click hear</a>
