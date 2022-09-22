7fc7443e036c59003d3415c8a0d2fd39a94a7867
remi.derathe <remi.derathe@gmail.com>
2022-09-22 16:31:15 +0200

    add useContext

--- src/components/list_event/ListEvents.jsx
+++ src/components/list_event/ListEvents.jsx
@@ -1,103 +1,83 @@
-import React, { useState, useEffect } from 'react'
-import './ListEvents.css'
+import React, { useState, useContext } from "react";
+import "./ListEvents.css";
 import EventSelected from "../event_selected/EventSelected";
-
+import EventContext from "../../context/EventContext";
 
 export default function ListEvents() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState(null);
-  const [item, setItem] = useState(null);
+  const [itemSelected, setItemSelected] = useState(null);
   const [viewState, setViewState] = useState(true);
-  const [start, setStart] = useState(0);
 
-  const URL = `https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=agenda-des-manifestations-culturelles-so-toulouse&q=&rows=10&start=${start}&timezone=europe%2FBerlin`;
-
-
-
-  function getUrl() {
-    fetch(URL)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result.records);
-
-        },
-        (err) => {
-          setIsLoaded(true);
-          setError(err);
-        }
-      )
-  }
-
-  function increment(start) {
-    setStart(start + 10);
-    getUrl();
-  }
-
-  function decrement(start) {
-    (start === 10) ? setStart(start) : setStart(start - 10);
-    getUrl();
-  }
+  const { items, increment, decrement } = useContext(EventContext);
 
   function handleClick(evnt) {
-    setItem(evnt);
+    setItemSelected(evnt);
     setViewState(false);
   }
 
-  function handleSlide(state) {
+  function handleSlide() {
     setViewState(!viewState);
-    setItem(null);
+    setItemSelected(null);
   }
-
-  useEffect(() => {
-    getUrl();
-  }, [])
-
-  if (error) {
-    return <div>Erreur : {error.message}</div>;
-  } else if (!isLoaded) {
+  if (!items) {
     return <div>Chargement...</div>;
-  } else {
+  }
+  if (items) {
     return (
-      <div className='listEvents'>
-
-
-        <ul className={viewState ? 'listEvents__ul' : 'listEvents__ul--reduced'}>
-
-          {items?.map(item => (
-            <li key={item.recordid} onClick={() => handleClick(item)} className='listEvents__li'>
-              <div className='listEvents__li--entete'>
+      <div className="listEvents">
+        <ul
+          className={viewState ? "listEvents__ul" : "listEvents__ul--reduced"}
+        >
+          {items?.map((item) => (
+            <li
+              key={item.recordid}
+              onClick={() => handleClick(item)}
+              className="listEvents__li"
+            >
+              <div className="listEvents__li--entete">
                 <div>
-                  <p>{(item.fields.nom_de_la_manifestation)}</p>
+                  <p>{item.fields.nom_de_la_manifestation}</p>
                 </div>
                 <div>
-                  <p>à partir du {new Date(item.fields.date_debut).toLocaleDateString()}</p>
+                  <p>
+                    à partir du{" "}
+                    {new Date(item.fields.date_debut).toLocaleDateString()}
+                  </p>
                   <p>à {item.fields.commune}</p>
                 </div>
               </div>
-              <p className={viewState ? 'listEvents__ul--p' : 'listEvents__ul--display'}>{(item.fields.descriptif_court)} (...)</p>
+              <p
+                className={
+                  viewState ? "listEvents__ul--p" : "listEvents__ul--display"
+                }
+              >
+                {item.fields.descriptif_court} (...)
+              </p>
             </li>
           ))}
-          <div>
-          </div>
+          <div />
 
-          <div className='listEvents__button'>
-            <button type='button' onClick={() => decrement(start)}> précédents </button>
-            <button type='button' onClick={() => increment(start)}> suivants </button>
+          <div className="listEvents__button">
+            <button type="button" onClick={() => decrement()}>
+              précédents
+            </button>
+            <button type="button" onClick={() => increment()}>
+              suivants
+            </button>
           </div>
         </ul>
 
-        <button className={viewState ? 'listEvents__ul--button' : 'ListEvents__slide--button'} onClick={() => handleSlide(viewState)} type='button'> ... </button>
+        <button
+          className={
+            viewState ? "listEvents__ul--button" : "ListEvents__slide--button"
+          }
+          onClick={() => handleSlide(viewState)}
+          type="button"
+        >
+          ...
+        </button>
 
-        <div >
-          {item ? <EventSelected item={item} /> : null}
-        </div>
-
+        <div>{itemSelected ? <EventSelected item={itemSelected} /> : null}</div>
       </div>
     );
   }
 }
-
-
